0ed6c533cbea217896d091191dac4c711896ebbf
sunmok <rlfhr9191@gmail.com>
2025-11-02 20:05:35 +0900

    feat:homework_11.7

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -150,6 +150,10 @@ void get_filetype(char *filename, char *filetype) {
         strcpy(filetype, "image/png");
     else if (strstr(filename, ".jpg"))
         strcpy(filetype, "image/jpeg");
+    else if (strstr(filename, ".mpg") || strstr(filename, ".mpeg"))
+        strcpy(filetype, "video/mpeg");
+    else if (strstr(filename, ".mp4"))
+        strcpy(filetype, "video/mp4");
     else
         strcpy(filetype, "text/plain");
 }
